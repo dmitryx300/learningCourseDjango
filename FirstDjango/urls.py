50300c057462ffe492d622e5b1296216ef7646dd
dmitryx300 <dmitryx300@gmail.com>
2023-07-12 17:33:50 +0200

    base template + include inc-menu

--- FirstDjango/urls.py
+++ FirstDjango/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='Home'),
     path('about/', views.about, name='About'),
-    path('item/<int:id>/', views.get_item),
+    path('item/<int:id>/', views.get_item, name='item-detail'),
     path('items/', views.items_list, name='Items')
 ]
