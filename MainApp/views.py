85c8f682c78327e08f7d4981a7e70bfbef304c39
dmitryx300 <dmitryx300@gmail.com>
2023-07-12 15:21:57 +0200

    Module 3 part 1 completed

--- MainApp/views.py
+++ MainApp/views.py
@@ -1,18 +1,20 @@
+from django.core.exceptions import ObjectDoesNotExist
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseNotFound
+from MainApp.models import Item
 
 # Create your views here.
 author = {'Имя': 'Иван', 'Фамилия': 'Петрович', 'Отчество': 'Иванов',
           'Телефон': '8-923-600-01-02', 'email': 'vasya@mail.ru'}
 
 
-items = [
-   {"id": 1, "name": "Кроссовки abibas", "quantity": 5},
-   {"id": 2, "name": "Куртка кожаная", "quantity": 2},
-   {"id": 5, "name": "Coca-cola 1 литр", "quantity": 12},
-   {"id": 7, "name": "Картофель фри", "quantity": 0},
-   {"id": 8, "name": "Кепка", "quantity": 124},
-]
+# items = [
+#    {"id": 1, "name": "Кроссовки abibas", "quantity": 5},
+#    {"id": 2, "name": "Куртка кожаная", "quantity": 2},
+#    {"id": 5, "name": "Coca-cola 1 литр", "quantity": 12},
+#    {"id": 7, "name": "Картофель фри", "quantity": 0},
+#    {"id": 8, "name": "Кепка", "quantity": 124},
+# ]
 menu = ['Home', 'Items', 'About']
 
 def home(request):
@@ -39,21 +41,30 @@ def about(request):
     return render(request, 'about.html', contex)
 
 def get_item(request, id):
-    for item in items:
-        if item['id'] == id:
-        #     text = f"""
-        #     <h2>Имя: {item['name']}</h2>
-        #     <p>quantity: {item['quantity']}</p>
-        #     <a href='/items'> Назад </a>
-        #     """
-        #     return HttpResponse(text)
-            context = {
-                'item': item,
-                'menu': menu,
-                'title': 'Item page'
-            }
-            return render(request, 'item-page.html', context)
-    return(HttpResponseNotFound(f'Item with id={id} not found'))
+    # for item in items:
+    #     if item['id'] == id:
+    #     #     text = f"""
+    #     #     <h2>Имя: {item['name']}</h2>
+    #     #     <p>quantity: {item['quantity']}</p>
+    #     #     <a href='/items'> Назад </a>
+    #     #     """
+    #     #     return HttpResponse(text)
+    #         context = {
+    #             'item': item,
+    #             'menu': menu,
+    #             'title': 'Item page'
+    #         }
+    try:
+        item = Item.objects.get(id=id)
+    except ObjectDoesNotExist:
+        return(HttpResponseNotFound(f'Item with id={id} not found'))
+    context = {
+        'item': item,
+        'menu': menu,
+        'title': 'Item page'
+    }
+    return render(request, 'item-page.html', context)
+
 
 def items_list(request):
     # result = "<h2>Список товаров</h2><ol>"
@@ -62,7 +73,7 @@ def items_list(request):
     # result += '</ol>'
     #return HttpResponse(result)
     context = {
-        'items': items,
+        'items': Item.objects.all(),
         'menu': menu,
         'title': 'Items main'
     }
